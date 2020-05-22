df69eee187137b85b0bba67c2fc251807b51369b
Slesa Adhikari <slesaad@gmail.com>
2020-05-21 20:06:01 -0500

    Updates

---
+++ precache-manifest.92a553bbaaa110429e4109f5b5484980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4b63740409145e33e959044239eb73",
+    "url": "/zygote/index.html"
+  },
+  {
+    "revision": "aa33fdbe030ac23cea65",
+    "url": "/zygote/static/css/main.e11888e5.chunk.css"
+  },
+  {
+    "revision": "a64426394485b3c4689c",
+    "url": "/zygote/static/js/2.f1f25122.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/zygote/static/js/2.f1f25122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa33fdbe030ac23cea65",
+    "url": "/zygote/static/js/main.2eea1014.chunk.js"
+  },
+  {
+    "revision": "e97de22fc26430fa220c",
+    "url": "/zygote/static/js/runtime-main.69c37813.js"
+  }
+]);
